df703867bea5e4aa6d7ad42273a2d390b807c9b9
ms4py <marc.schlaich@googlemail.com>
2010-03-07 12:45:12 +0100

    new description

--- latexmk.py
+++ latexmk.py
@@ -5,8 +5,7 @@
 Latexmk.py completely automates the process of generating 
 a LaTeX document. Given the source files for a document, 
 latexmk.py issues the appropriate sequence of commands to 
-generate a .dvi or .pdf version of the document.  It can 
-also be set to run continuously with a previewer.
+generate a .dvi or .pdf version of the document.
 
 
 Inspired by http://ctan.tug.org/tex-archive/support/latexmk/
