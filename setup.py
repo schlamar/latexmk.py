c980426bffae20eec867f3f4be724776cfd0bfe2
ms4py <marc.schlaich@googlemail.com>
2011-03-21 18:11:36 +0100

    setup fix for python3

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from setuptools import setup
 
-import latexmk
-
+        
 setup(
       name = 'latexmk.py',      
-      version = latexmk.__version__, 
+      version = '0.2', 
       description=('Latexmk.py completely automates the process of '
                    'generating a LaTeX document.'),
       long_description=('Latexmk.py completely automates the process of '
@@ -14,10 +13,10 @@ setup(
                         'for a document, latexmk.py issues the appropriate '
                         'sequence of commands to generate a .dvi or .pdf '
                         'version of the document.'),
-      author=latexmk.__author__,  
+      author='Marc Schlaich',  
       author_email='marc.schlaich@googlemail.com',
       url='http://bitbucket.org/ms4py/latexmk.py/',
-      license=latexmk.__license__,
+      license='MIT',
       platforms = 'any',
       classifiers=['Development Status :: 4 - Beta',
                    'Intended Audience :: End Users/Desktop',
@@ -26,6 +25,8 @@ setup(
                    'Programming Language :: Python',
                    'Topic :: Printing',
                    'Topic :: Text Processing :: Markup :: LaTeX'],
-      scripts = ['latexmk.py'],
+                   
+      py_modules = ['latexmk'],
+      entry_points = {'console_scripts': ['latexmk = latexmk:main']},
       use_2to3 = True
       )
